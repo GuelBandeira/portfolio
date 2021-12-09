7afb63a0de2ed70995e91c6c6f65d5ddd583b192
GuelBandeira <bandeiraguel@gmail.com>
2021-12-09 09:19:09 -0300

    v0.2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
+import BlurBackground from "./components/BlurBackground";
 import TypingEffect from "./components/TypingEffect";
 
 function App() {
   return (
     <>
-      <div className="">
-        <TypingEffect />
+      <div class="relative z-20 hidden grid-cols-2 lg:grid">
+        <BlurBackground />
+        <div class="relative z-10 min-h-screen mr-10 bg-black bg-opacity-40 min-w-screen max-w-none">
+          <div class="flex items-center justify-center mt-96 text-purple-500 text-6xl ">
+            <TypingEffect />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/BlurBackground.jsx
+++ src/components/BlurBackground.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BlurBackground = () => {
   return (
-    <div class="bg-gray-50 min-h-screen flex items-center justify-center px-16">
+    <div class="bg-gray-50 min-h-screen absolute flex items-center justify-center px-16">
       <div class="relative w-full max-w-lg">
         <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
         <div class="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
