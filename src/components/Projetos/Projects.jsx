b0cafd3eee70b801a8d5d68e5ea08ee0b0cf1486
GuelBandeira <bandeiraguel@gmail.com>
2022-09-20 08:16:48 -0300

    0.7.6

--- src/components/Projetos/Projects.jsx
+++ src/components/Projetos/Projects.jsx
@@ -12,12 +12,14 @@ import "./Projects.css";
 import IconProjects from "./IconProjects";
 import Video from "./Video";
 import etpc from "../../images/etpcesports.png";
-import shopping from "../../images/shopping33.png";
+import shopping from "../../images/mycar.png";
+import formulario from "../../images/formulario.png";
+import bitway from "../../images/bitway.png";
 const Projects = () => {
   return (
     <>
       {/* PROJETOS */}
-      <main className="imagem-fundo pt-48 lg:-mb-28">
+      <main className=" pt-48 lg:-mb-28">
         {/* HEADER */}
         <h1
           className="text-white text-center text-6xl"
@@ -25,8 +27,7 @@ const Projects = () => {
         >
           Alguns de meus projetos:
         </h1>
-
-        <section className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-4 py-12 text-white">
+        <section className="col-span-6 max-w-3xl mx-auto px-4 sm:px-6 lg:px-4 py-12 text-white">
           {/* <Video
             linkVideo={"https://www.youtube.com/embed/LhSDtX5HCHQ"}
           ></Video> */}
@@ -58,19 +59,15 @@ const Projects = () => {
               <span className="p-4">
                 <IconProjects>
                   <FaCheckCircle className="inline-flex mr-2 mb-1" />
-                  Criação completa do Front-end deste Website
+                  Front-end
                 </IconProjects>
                 <IconProjects>
                   <FaCheckCircle className="inline-flex mr-2 mb-1" />
-                  Criação completa do Back-end deste Website
+                  Back-end
                 </IconProjects>
                 <IconProjects>
                   <FaCheckCircle className="inline-flex mr-2 mb-1" />
                   UX/UI Design
-                </IconProjects>
-                <IconProjects>
-                  <FaCheckCircle className="inline-flex mr-2 mb-1" />
-                  Marketing
                 </IconProjects>
               </span>
             </p>
@@ -93,8 +90,7 @@ const Projects = () => {
             </div>
           </div>
         </section>
-
-        <section className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-4 py-12 text-white">
+        <section className="col-span-6 max-w-3xl mx-auto px-4 sm:px-6 lg:px-4 py-12 text-white">
           {/* <Video
             linkVideo={"https://www.youtube.com/embed/LhSDtX5HCHQ"}
           ></Video> */}
@@ -110,37 +106,141 @@ const Projects = () => {
               className=" text-center text-3xl font-bold pt-32 font"
               data-aos="flip-left"
             >
-              SHOPPING 33
+              MY NFT CAR
             </h1>
             <p
               className="text-justify font-thin max-w-xl pt-9 px-9 lg:p-0 font"
               data-aos="fade-zoom-in"
             >
-              Atualmente estou trabalhando no site do Shopping33 durante a minha
-              estadia como Desenvolvedor Full Stack na agência Dec9 - Makerting
-              e Tecnologia. Estou desempenhando as seguintes funções no projeto:
+              O MY NFT CAR é um dos projetos dos quais mais me orgulho de ter feito, pois foi muito gratificante de construir o website tentando ao máximo me desafiar e deixá-lo o mais bonito e fiel á proposta possível:
               <br></br>
               <span className="p-4">
                 <IconProjects>
                   <FaCheckCircle className="inline-flex mr-2 mb-1" />
-                  Criação completa do Front-end deste Website
-                </IconProjects>
-                <IconProjects>
-                  <FaCheckCircle className="inline-flex mr-2 mb-1" />
-                  UI/UX Design
+                  Front-end
                 </IconProjects>
               </span>
             </p>
 
             <span className="inline-flex text-4xl text-white space-x-5 text-center items-center justify-items-center">
-              <FaHtml5 /> <FaCss3 /> <FaPhp />
-              <SiJavascript />
+              <FaHtml5 /> <SiJavascript /><FaCss3 /> 
+              
             </span>
             <div className="block pb-10 pt-2">
               <a
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://shopping33.com.br/"
+                href="https://mynftcar.io/"
+              >
+                <button className="item3 button hover:shadow-2xl text-indigo-600 hover:shadow-black bg-white rounded-xl text-lg p-3 relative  justify-center overflow-hidden duration-1000 transform ease-in-out hover:text-white  ">
+                  <RiShareBoxFill className="inline-flex mb-1 mr-1 font-bold" />
+                  Acessar Site
+                </button>
+              </a>
+            </div>
+          </div>
+        </section>
+        <section className="col-span-6 max-w-3xl mx-auto px-4 sm:px-6 lg:px-4 py-12 text-white">
+          {/* <Video
+            linkVideo={"https://www.youtube.com/embed/LhSDtX5HCHQ"}
+          ></Video> */}
+          <div className="embed-container mx-auto self-center max-w-2xl absolute shadow z-50 rounded-2xl">
+            <img
+              src={formulario}
+              className="rounded-2xl border-4 mx-auto border-indigo-600"
+              data-aos="fade-up"
+            ></img>
+          </div>
+          <div className="w-full bg-indigo-600 shadow-indigo-900 shadow-2xl rounded-lg -mt-24 flex space-y-5 flex-col justify-center items-center">
+            <h1
+              className=" text-center text-3xl font-bold pt-32 font"
+              data-aos="flip-left"
+            >
+              NYX SOUL
+            </h1>
+            <p
+              className="text-justify font-thin max-w-xl pt-9 px-9 lg:p-0 font"
+              data-aos="fade-zoom-in"
+            >
+              O MY NFT CAR é um dos projetos dos quais mais me orgulho de ter feito, pois foi muito gratificante de construir o website tentando ao máximo me desafiar e deixá-lo o mais bonito e fiel á proposta possível:
+              <br></br>
+              <span className="p-4">
+                <IconProjects>
+                  <FaCheckCircle className="inline-flex mr-2 mb-1" />
+                  Front-end
+                </IconProjects>
+                <IconProjects>
+                  <FaCheckCircle className="inline-flex mr-2 mb-1" />
+                  Back-end
+                </IconProjects>
+              </span>
+            </p>
+
+            <span className="inline-flex text-4xl text-white space-x-5 text-center items-center justify-items-center">
+              <FaHtml5 /> <SiJavascript /><FaCss3 /> 
+              <FaPhp />
+              <FaDatabase />
+            </span>
+            <div className="block pb-10 pt-2">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://mynftcar.io/"
+              >
+                <button className="item3 button hover:shadow-2xl text-indigo-600 hover:shadow-black bg-white rounded-xl text-lg p-3 relative  justify-center overflow-hidden duration-1000 transform ease-in-out hover:text-white  ">
+                  <RiShareBoxFill className="inline-flex mb-1 mr-1 font-bold" />
+                  Acessar Site
+                </button>
+              </a>
+            </div>
+          </div>
+        </section>
+        <section className="col-span-6 max-w-3xl mx-auto px-4 sm:px-6 lg:px-4 py-12 text-white">
+          {/* <Video
+            linkVideo={"https://www.youtube.com/embed/LhSDtX5HCHQ"}
+          ></Video> */}
+          <div className="embed-container mx-auto self-center max-w-2xl absolute shadow z-50 rounded-2xl">
+            <img
+              src={bitway}
+              className="rounded-2xl border-4 mx-auto border-indigo-600"
+              data-aos="fade-up"
+            ></img>
+          </div>
+          <div className="w-full bg-indigo-600 shadow-indigo-900 shadow-2xl rounded-lg -mt-24 flex space-y-5 flex-col justify-center items-center">
+            <h1
+              className=" text-center text-3xl font-bold pt-32 font"
+              data-aos="flip-left"
+            >
+              BITWAY
+            </h1>
+            <p
+              className="text-justify font-thin max-w-xl pt-9 px-9 lg:p-0 font"
+              data-aos="fade-zoom-in"
+            >
+              O MY NFT CAR é um dos projetos dos quais mais me orgulho de ter feito, pois foi muito gratificante de construir o website tentando ao máximo me desafiar e deixá-lo o mais bonito e fiel á proposta possível:
+              <br></br>
+              <span className="p-4">
+                <IconProjects>
+                  <FaCheckCircle className="inline-flex mr-2 mb-1" />
+                  Front-end
+                </IconProjects>
+                <IconProjects>
+                  <FaCheckCircle className="inline-flex mr-2 mb-1" />
+                  Back-end
+                </IconProjects>
+              </span>
+            </p>
+
+            <span className="inline-flex text-4xl text-white space-x-5 text-center items-center justify-items-center">
+              <FaHtml5 /><SiJavascript /> <FaCss3 /> 
+              <FaPhp />
+              <FaDatabase />
+            </span>
+            <div className="block pb-10 pt-2">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://mynftcar.io/"
               >
                 <button className="item3 button hover:shadow-2xl text-indigo-600 hover:shadow-black bg-white rounded-xl text-lg p-3 relative  justify-center overflow-hidden duration-1000 transform ease-in-out hover:text-white  ">
                   <RiShareBoxFill className="inline-flex mb-1 mr-1 font-bold" />
